42810e59ba8be75034fa0d3fcb601bc892bba777
amirhosein <amirhosein@cyberdesign.se>
2023-10-14 22:11:12 +0330

    feat: add customer data model

---
+++ src/main/java/com/example/datajpa/model/Customer.java
@@ -0,0 +1,34 @@
+package com.example.datajpa.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Customer")
+@Table(name = "customer")
+@Getter
+@NoArgsConstructor
+public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_sequence",
+            sequenceName = "customer_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_sequence"
+    )
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column
+    private String address;
+}
